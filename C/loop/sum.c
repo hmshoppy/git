ffe38291b0f1a40955131d750a1a4d1aab1ec048
hmshoppy2015 <hmshoppy2015@gmail.com>
2016-10-24 10:24:08 +0800

    add C/loop

---
+++ C/loop/sum.c
@@ -0,0 +1,22 @@
+
+/*sum a series of numbers */
+
+#include <stdio.h>
+
+int main()
+{
+	int n, sum = 0;
+
+	printf("This program sums a series of integers.\n");
+
+	printf("Enter integers(0 to teminate): ");
+
+	scanf("%d", &n);
+	while(n != 0) {
+		sum += n;
+		scanf("%d", &n);
+	}
+
+	printf("The sum is: %d\n", sum);
+	return 0;
+}
